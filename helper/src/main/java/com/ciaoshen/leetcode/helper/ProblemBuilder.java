b6584fdbb67ecef05fe396db227e17426886b4cd
helloShen <symantec__@hotmail.com>
2018-09-07 01:39:43 -0400

    velocity installed and path build

--- helper/src/main/java/com/ciaoshen/leetcode/helper/ProblemBuilder.java
+++ helper/src/main/java/com/ciaoshen/leetcode/helper/ProblemBuilder.java
@@ -4,18 +4,65 @@
  * @author Pixel SHEN
  */
 package com.ciaoshen.leetcode.helper;
+// java.io
 import java.io.File;
+import java.io.Writer;
 import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
 import java.io.FileNotFoundException;
 import java.io.IOException;
+// velocity
+import org.apache.velocity.app.VelocityEngine;
+import org.apache.velocity.Template;
+import org.apache.velocity.VelocityContext;
 
 
 public class ProblemBuilder {
 
-    /** 读取整个文件 */
+    public ProblemBuilder(String[] args) {
+        if (args.length != 4) {
+            throw new IllegalArgumentException("Must have 4 arguments!");
+        }
+        templateDir = args[0];
+        srcDir = args[1];
+        packagePath = args[2].replaceAll("\\.","/");
+        problemName = args[3];
+        problemDir = srcDir + "/" + packagePath + "/" + problemName;
+        ve = new VelocityEngine();
+        ve.init();
+    }
+    /**
+     * 针对一个
+     * @param path [description]
+    void writeTemplate(String fileName) {
+        Template t = ve.getTemplate(templatePath);
+        VelocityContext context = new VelocityContext();
+        context.put("package", problemName);
+        Writer w = getWriter(targetPath);
+        t.merge(context, w);
+        w.write(w.toString());
+        try {
+            w.close();
+        } catch (IOException ioe) {
+            throw new RuntimeException("ProblemBuilder#writeTemplate() can not close the Writer!");
+        }
+    }
+     */
+
+    /** 获取一个用BufferedWriter装饰的FileWriter */
+    Writer getWriter(String path) {
+        try {
+            return new BufferedWriter(new FileWriter(new File(path)));
+        } catch (FileNotFoundException fnfe) { // new File()
+            throw new RuntimeException("File not found: <" + path + ">.");
+        } catch (IOException ioe) { // new FileWriter()
+            throw new RuntimeException("IOException occurred when creating the new FileWriter.");
+        }
+    }
+
+    /** 读取指定文本文件 */
     String readFile(String path) {
         StringBuilder sb = new StringBuilder();
         try {
@@ -36,4 +83,37 @@ public class ProblemBuilder {
         return sb.toString();
     }
 
+    String getProblemDir() {
+        return problemDir;
+    }
+
+    private VelocityEngine ve;
+    private String templateDir;             // 模板所在根目录
+    private String srcDir;                  // 项目源代码根目录
+    private String packagePath;             // 像com.ciaoshen.leetcode.helper这样的包名
+    private String problemName;             // 问题名字
+    // 以[src_dir/package_path/problem_name]格式构成的项目源码目录
+    private String problemDir;
+
+    /**
+     * Must have 4 arguments:
+     *      1. templateDir
+     *      2. srcDir
+     *      3. package
+     *      4. problemName
+     *
+     * 首先，参数传进来的"package"只是个包名："com.ciaoshen.leetcode.helper"，
+     * 需要转换成 "com/ciaoshen/leetcode/helper"样式的路径：packagePath
+     *
+     * 这些参数被用来构造我们生成的文件的目标路径：
+     *
+     *      problemDir = [srcDir/packagePath/problemName]
+     *
+     * 最后在templateDir里有几个模板文件，就在problemDir里填充这几个模板.
+     */
+    public static void main(String[] args) {
+        if (args.length != 4) {
+            throw new IllegalArgumentException("Must have 4 argument!");
+        }
+    }
 }
--- helper/src/test/java/com/ciaoshen/leetcode/helper/ProblemBuilderTest.java
+++ helper/src/test/java/com/ciaoshen/leetcode/helper/ProblemBuilderTest.java
@@ -8,7 +8,13 @@ package com.ciaoshen.leetcode.helper;
 import static org.junit.Assert.*;
 import org.junit.Before;
 import org.junit.Test;
+/** slf4j */
+import org.slf4j.impl.StaticLoggerBinder;
+import org.slf4j.LoggerFactory;
+/** commons-lang3 */
+import org.apache.commons.lang3.StringUtils;
 /** java.io */
+import java.io.Writer;
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
 import java.io.FileNotFoundException;
@@ -17,18 +23,43 @@ import java.io.IOException;
 public class ProblemBuilderTest {
 
     public ProblemBuilderTest() {
-        builder = new ProblemBuilder();
+        builder = new ProblemBuilder(ARGS);
+        assertNotNull(builder);
+        assertEquals("/Users/Wei/github/leetcode/helper/src/main/java/com/ciaoshen/leetcode/helper/two_sum", builder.getProblemDir());
+        System.out.println("ProblemBuilder constructor pass JUnit test!");
+        System.out.println("ProblemBuilder#getProblemDir() method pass JUnit test!");
+    }
+    @Test
+    public void testGetWriter() {
+        Writer w = builder.getWriter(W_TEST);
+        assertNotNull(w);
+        System.out.println("ProblemBuilder#getWriter() method pass JUnit test!");
+        try {
+            w.close();
+        } catch (IOException ioe) {
+            throw new RuntimeException("ProblemBuilderTest#testGetWriter() can not close the FileWriter!");
+        }
     }
     @Test
     public void testReadFile() {
-        String content = builder.readFile(F_TEST);
-        assertEquals(content, "Hello Ronald!");
+        String content = builder.readFile(R_TEST);
+        assertEquals("Hello Ronald!", content);
         System.out.println("ProblemBuilder#readFile() method pass JUnit test!");
     }
 
     /**==================== 【 private 】 =========================*/
+    /** 根目录 */
     private final String ROOT = "/Users/Wei/github/leetcode/helper";
+    /** 构造ProblemBuilder的4个必要参数 */
+    private final String SRC_DIR = ROOT + "/src/main/java";
+    private final String TPL_DIR = ROOT + "/tpl";
+    private final String PCK = "com.ciaoshen.leetcode.helper";
+    private final String PROB = "two_sum";
+    private final String[] ARGS = new String[]{TPL_DIR, SRC_DIR, PCK, PROB};
+    /** 测试FileReader的文本文件 */
     private final String RES_DIR = ROOT + "/src/test/resources";
-    private final String F_TEST = RES_DIR + "/reader_test.txt";
+    private final String R_TEST = RES_DIR + "/reader_test.txt";
+    private final String W_TEST = RES_DIR + "/writer_test.txt";
+    /** 内置ProlbemBuilder */
     private ProblemBuilder builder;
 }
