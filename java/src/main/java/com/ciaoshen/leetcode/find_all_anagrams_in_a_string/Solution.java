39b5dc04fb64c535ad2d035783264d9ef7c7840d
helloShen <symantec__@hotmail.com>
2018-10-05 17:11:12 -0400

    update find all anagrams in a string

--- java/src/main/java/com/ciaoshen/leetcode/find_all_anagrams_in_a_string/Solution.java
+++ java/src/main/java/com/ciaoshen/leetcode/find_all_anagrams_in_a_string/Solution.java
@@ -5,8 +5,14 @@ package com.ciaoshen.leetcode.find_all_anagrams_in_a_string;
 import java.util.*;
 import com.ciaoshen.leetcode.util.*;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 interface Solution {
 
+    // use this Object to print the log (call from slf4j facade)
+    static Logger log = LoggerFactory.getLogger(Solution.class);
+    
     public List<Integer> findAnagrams(String s, String p);
 
 }
