a76ded81e97444c0a17f0ddc5cea8f5f9e8c106d
helloShen <symantec__@hotmail.com>
2018-12-11 21:03:33 -0500

    leetcode update heaters

--- java/src/main/java/com/ciaoshen/leetcode/heaters/Solution2.java
+++ java/src/main/java/com/ciaoshen/leetcode/heaters/Solution2.java
@@ -17,7 +17,34 @@ import com.ciaoshen.leetcode.util.*;
 class Solution2 implements Solution {
 
     public int findRadius(int[] houses, int[] heaters) {
-        return 2;
+        if (houses.length == 0 || heaters.length == 0) return 0;
+        Arrays.sort(houses);
+        Arrays.sort(heaters);
+        int radius = 0;
+        for (int house : houses) {
+            int idx = Arrays.binarySearch(heaters, house);
+            if (idx >= 0) continue;
+            idx = - (idx + 1);
+            if (log.isDebugEnabled()) {
+                log.debug("House {} should insert at {} in heater array.", house, idx);
+            }
+            int leftRadius = (idx == 0)? -1 : house - heaters[idx - 1];
+            int rightRadius = (idx == heaters.length)? -1 : heaters[idx] - house;
+            if (log.isDebugEnabled()) {
+                log.debug("So left radius = {}, right radius = {}", leftRadius, rightRadius);
+            }
+            if (leftRadius == -1) {
+                radius = Math.max(radius, rightRadius);
+                continue;
+            }
+            if (rightRadius == -1) {
+                radius = Math.max(radius, leftRadius);
+                continue;
+            }
+            int currRadius = Math.min(leftRadius, rightRadius);
+            radius = Math.max(radius, currRadius);
+        }
+        return radius;
     }
 
 }
